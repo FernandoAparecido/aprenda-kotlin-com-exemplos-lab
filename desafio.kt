a8eee908ce18c1898d7f061ded19d796a7bcbb70
Fernando Silva <113550767+FernandoAparecido@users.noreply.github.com>
2023-01-12 23:19:01 -0300

    realizadodesafio.kt

--- desafio.kt
+++ desafio.kt
@@ -2,29 +2,41 @@
 
 enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
 
-class Usuario(varag nome: String){
+class Usuario(vararg nome: String) {
 
-}
+    }
 
-class ConteudoEducacional(varag nome: String, var duracao: John)
+class ConteudoEducacional(vararg nome: String, var duracao: John)
+
 
 data class Formacao(var nome: Usuario, var conteudos: List<ConteudoEducacional>) {
 
     val inscritos = mutableListOf<Usuario>(nome)
-    
+
     fun matricular(usuario: Usuario) {
+
         inscritos.add(usuario)
     }
 
+
+
+    fun mostrarinscritos() {
+        inscritos.forEach { println(it) }
+
+    }
 }
 
 fun main() {
-  val content = listof(ConteudoEducacional("Kotlin Experience", duracao = "40 HORAS"))
+    val content = listOf(ConteudoEducacional("Kotlin Experience", duracao = "40 Horas"))
 
-  val users = Usuario("Paulo","Marcio","Pedro")
+    val users = Usuario("Paulo","João", "Pedro")
 
-   val formacao = Formacao(users, content)
+
+    val formacao = Formacao(users, content)
     formacao.matricular(users)
     val inscr = (formacao.inscritos)
 
     formacao.mostrarinscritos()
+
+
+}
