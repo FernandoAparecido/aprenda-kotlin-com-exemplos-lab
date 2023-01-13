0a7dc6767aac3f03df26f1f56a3fd3eda348be25
Fernando Silva <113550767+FernandoAparecido@users.noreply.github.com>
2023-01-12 23:09:54 -0300

    realizadodesafio.kt

--- desafio.kt
+++ desafio.kt
@@ -2,20 +2,29 @@
 
 enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
 
-class Usuario
+class Usuario(varag nome: String){
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+}
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+class ConteudoEducacional(varag nome: String, var duracao: John)
 
-    val inscritos = mutableListOf<Usuario>()
+data class Formacao(var nome: Usuario, var conteudos: List<ConteudoEducacional>) {
+
+    val inscritos = mutableListOf<Usuario>(nome)
     
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        inscritos.add(usuario)
     }
+
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
-}
+  val content = listof(ConteudoEducacional("Kotlin Experience", duracao = "40 HORAS"))
+
+  val users = Usuario("Paulo","Marcio","Pedro")
+
+   val formacao = Formacao(users, content)
+    formacao.matricular(users)
+    val inscr = (formacao.inscritos)
+
+    formacao.mostrarinscritos()
